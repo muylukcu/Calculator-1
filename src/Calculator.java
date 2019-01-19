9ae464ba6cc236e11239efb8ab45e768cf247062
github@cybertekschool.com <github@cybertekschool.com>
2019-01-19 17:02:54 -0500

    LOCAL: Add divide method

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,4 +13,9 @@ public class Calculator {
         return num1 * num2;
     }
 
+    public static double divide(double n1, double n2) {
+        double result = n1 / n2;
+        return result;
+    }
+
 }
