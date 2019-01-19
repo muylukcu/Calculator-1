c3132690102c614e42e7f6c55a6c456b9a58e329
github@cybertekschool.com <github@cybertekschool.com>
2019-01-19 17:06:58 -0500

    merged

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,11 +13,6 @@ public class Calculator {
         return num1 * num2;
     }
     
-    public static double divide(double num1, double num2) {
-        return num1 / num2;
-    }
-
-
     public static double divide(double n1, double n2) {
         double result = n1 / n2;
         return result;
